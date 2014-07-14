760b5970273afcd86618a95ee5a15aa6fe70f04f
= <=>
2014-07-14 15:22:27 +0200

    Initial commit

---
+++ com/github/phumi/whitebelt2/App.java
@@ -0,0 +1,20 @@
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+
+public class App{
+
+	public static void main(String[]args){
+	
+		LocalDateTime now = LocalDateTime.now();
+
+		System.out.println("It is now: " + now);
+	
+		LocalDateTime nowTenMinutesLater = now.plus(10, ChronoUnit.MINUTES);
+	
+		System.out.println("Now did not change, it is immutable will never change:" + now);
+		System.out.println("Ten minutes from now is: " + nowTenMinutesLater);
+	
+	}
+}
